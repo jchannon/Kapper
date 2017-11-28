721fb5a4ca1c13d40aac130dc98766e8da602b70
Jonathan Channon <jonathan.channon@gmail.com>
2017-11-28 21:47:22 +0000

    made query/queryfirst extensions of Connection and passed in sql string to it

--- src/Kapper.kt
+++ src/Kapper.kt
@@ -1,58 +1,96 @@
+import java.sql.Connection
 import java.sql.DriverManager
-import javax.sql.DataSource
+import java.util.*
 import kotlin.reflect.KClass
 import kotlin.reflect.KMutableProperty
 import kotlin.reflect.full.createInstance
 import kotlin.reflect.full.memberProperties
-import kotlin.reflect.jvm.reflect
 
-class Kapper {
-    protected fun <T : Any> get(c: KClass<T>): T {
+//class Kapper {
+fun <T : Any> get(c: KClass<T>, sql: String, conn: Connection): List<T> {
 
-        var instance = c.createInstance()
+    //val g = hashMapOf("int8" to "Int")
 
-        val conn = DriverManager.getConnection("jdbc:postgresql:amdb")
-        val statement = conn.createStatement()
-        val reader = statement.executeQuery("select * from system")
-        reader.next()
+    var results = mutableListOf<T>()
 
+    var instance = c.createInstance()
 
+    val statement = conn.createStatement()
+    val reader = statement.executeQuery(sql)
+    
+    while (reader.next()) {
         c.memberProperties
                 .filterIsInstance<KMutableProperty<*>>()
                 .forEach {
-                    println(it.name)
-                    //while (reader.next()) {
-
                     for (i in 1..reader.metaData.columnCount) {
                         var colName = reader.metaData.getColumnName(i)
+                        // println(reader.metaData.getColumnTypeName(i))
                         if (colName.equals(it.name, true)) {
-                            it.setter.call(instance, reader.getString(colName))
+                            //println("found " + it.name + " & " + colName)
+
+                            when (reader.metaData.getColumnTypeName(i)) {
+                                "varchar" -> it.setter.call(instance, reader.getString(colName))
+                                "int8" -> it.setter.call(instance, reader.getInt(colName))
+                                "int4" -> it.setter.call(instance, reader.getInt(colName))
+                                "serial" -> it.setter.call(instance, reader.getInt(colName))
+                                "timestamp" -> it.setter.call(instance, reader.getDate(colName))
+                                "bool" -> it.setter.call(instance, reader.getBoolean(colName))
+                            }
+
+                            break
                         }
                     }
-                    //}
-
                 }
-
-        return instance
     }
 
-    inline fun <reified T : Any> get() = get(T::class)
+    results.add(instance)
+
+    return results
 }
+
+//inline fun <reified T : Any> get(sql: String) = get(T::class, sql)
+
+inline fun <reified T : Any> Connection.query(sql: String) = get(T::class, sql, this)
+inline fun <reified T : Any> Connection.queryfirst(sql: String) = get(T::class, sql, this).first()
+
+//}
 
 class Foo {
     var myname: String = "poo"
     var other: String = "jkl"
     var version: String = "empty"
+    var id: Int = 0
+    var lastportusageupdate: Date = Date()
+}
+
+class Setting {
+    var Id: Int = 0
+    var Name: String = ""
+    var Value: String = ""
+    var DefaultValue: String = ""
+    var Description: String = ""
+    var Visible: Boolean = false
+
 }
 
 fun main(args: Array<String>) {
 
-    val kapper = Kapper()
-    val foo = kapper.get<Foo>()
+    val conn = DriverManager.getConnection("jdbc:postgresql:amdb")
 
-    println(foo.myname)
-    println(foo.other)
-    println(foo.version)
+    val bar = conn.query<Setting>("select * from setting")
+    println(bar.first().Id)
+    println(bar.first().Name)
+    println(bar.first().Value)
+    println(bar.first().DefaultValue)
+    println(bar.first().Description)
+    println(bar.first().Visible)
 
+    var setting = conn.queryfirst<Setting>("select * from setting")
+    println(setting.Id)
+    println(setting.Name)
+    println(setting.Value)
+    println(setting.DefaultValue)
+    println(setting.Description)
+    println(setting.Visible)
 }
 
